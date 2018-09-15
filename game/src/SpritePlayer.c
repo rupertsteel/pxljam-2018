0830347987faebdf216ad9c7b6293fc5a8f37de6
Rupert Steel <rupert@rupertsteel.me>
2018-09-15 13:25:39 +1200

    Added animation.

--- game/src/SpritePlayer.c
+++ game/src/SpritePlayer.c
@@ -5,21 +5,31 @@ UINT8 bank_SPRITE_PLAYER = 2;
 #include "Keys.h"
 #include "SpriteManager.h"
 
+const UINT8 anim_idle[] = {1, 0};
+const UINT8 anim_walk[] = {2, 1, 2};
+
 void Start_SPRITE_PLAYER() {
 }
 
 void Update_SPRITE_PLAYER() {
     if (KEY_PRESSED(J_UP)) {
         THIS->y--;
+        SetSpriteAnim(THIS, anim_walk, 15);
     }
     if (KEY_PRESSED(J_DOWN)) {
         THIS->y++;
+        SetSpriteAnim(THIS, anim_walk, 15);
     }
     if (KEY_PRESSED(J_LEFT)) {
         THIS->x--;
+        SetSpriteAnim(THIS, anim_walk, 15);
     }
     if (KEY_PRESSED(J_RIGHT)) {
         THIS->x++;
+        SetSpriteAnim(THIS, anim_walk, 15);
+    }
+    if (keys == 0) {
+        SetSpriteAnim(THIS, anim_idle, 15);
     }
 }
 
