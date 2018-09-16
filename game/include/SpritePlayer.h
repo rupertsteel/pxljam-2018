5720597b1ab1ea678c78c078ebd9e44c3a877893
Sanjay Govind <govindsanj@myvuw.ac.nz>
2018-09-16 17:20:05 +1200

    Load controller id, change colour of main menu

--- game/include/SpritePlayer.h
+++ game/include/SpritePlayer.h
@@ -4,7 +4,7 @@
 typedef struct PlayerInfo {
     // 0 is faceing left, clockwise up to 3
     // 4 is facing up left, clockwise up to 7
-	UINT8 laser;
+	UINT8 controller;
     UINT8 hasControl;
     UINT8 near;
 } PlayerInfo;
